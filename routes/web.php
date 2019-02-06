645befb0ea3af0d00ac103c4b912f13aa73fc258
Carlos Mendez <carlos.mendez.marquez@gmail.com>
2019-02-06 16:24:38 -0400

    Agregado el algoritmo para dibujar el organigrama, y seeders para las tablas ma_cargo y ma_funcionario

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('profile', function () {
 
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/cargos', 'CargoController@getEstructura')->name('getEstructura');
