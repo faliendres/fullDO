14504d0725c726193fb4377c1721e880bfa7e7f3
F Aliendres <freddy.aliendres@gmail.com>
2019-02-08 14:16:35 -0400

    show search button

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,7 +10,17 @@
             {
                 "data": "id", "title": "Acciones",
                 "render": function (data, row) {
-                    return `<button class="btn btn-danger" data-id="${data}"><i class="fa fa-times"></i></button>`;
+                    let route = "{{route("users.show",["_id"])}}".replace("_id", data);
+                    return `
+                    <div class="btn-group">
+    <a class="btn btn-primary" data-id="${data}" href="${route}">
+        <i class="fa fa-search"></i>
+    </a>
+    <button class="btn btn-danger" data-id="${data}">
+        <i class="fa fa-times"></i>
+    </button>
+</div>
+`;
                 }
             },
         ];
