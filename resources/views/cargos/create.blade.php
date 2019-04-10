d85066cf4032ec5d5356d5d6aac92110b3ce2d92
Evelyn Ruiz <evelynruiz15@gmail.com>
2019-04-10 19:28:01 +0000

    add all jefaturas to create cargo

--- resources/views/cargos/create.blade.php
+++ resources/views/cargos/create.blade.php
@@ -11,10 +11,7 @@
         $gerencias=toOptions(\App\Gerencia::query()->where("id_empresa",$empresas->first()["id"]));
     else
         $gerencias=collect([]);
-    if($gerencias->count()===1)
-        $cargos=toOptions(\App\Cargo::query()->where("id_gerencia",$gerencias->first()["id"]));
-    else
-        $cargos=collect([]);
+    $options=\App\Cargo::options();
 @endphp
 
 @section("form")
@@ -28,7 +25,45 @@
     @include("partials.select",["required"=>true,"name"=>"id_holding","title"=>"Holding","stable"=>$user->perfil>0,"options"=>$holdings ])
     @include("partials.select",["required"=>true,"name"=>"id_empresa","title"=>"Empresa","stable"=>$user->perfil>1,"options"=>$empresas ])
     @include("partials.select",["required"=>true,"name"=>"id_gerencia","title"=>"Gerencia","stable"=>$user->perfil>2,"options"=>$gerencias ])
-    @include("partials.select",["name"=>"id_jefatura","title"=>"Jefatura","options"=>$cargos ])
+
+
+    @php
+        $auxId=uniqid("id_jefatura");
+    @endphp
+    <div class="row form-group">
+        <div class="col col-md-3">
+            <label for="{{$auxId}}" class=" form-control-label">Jefatura</label></div>
+        <div class="col-12 col-md-9">
+            <select name="id_jefatura" id="{{$auxId}}" class="form-control-lg form-control {{ $errors->has("id_jefatura") ? ' is-invalid' : '' }}">
+                <option selected value="" {{(isset($required)&&$required)?"disabled":""}} >{{$placeholder??"Seleccione por favor"}}
+                </option>
+                @foreach($options as $holding_id=>$empresas)
+                    <optgroup label="{{$holding_id}}">
+                        @foreach($empresas as $empresa_id=>$gerencias)
+                            <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;{{$empresa_id}}">
+                                @foreach($gerencias as $gerencia_id=>$cargos)
+                                    <optgroup label="&nbsp;&nbsp;&nbsp;&nbsp;{{$gerencia_id}}">
+                                        @foreach($cargos as $cargo)
+                                            <option value="{{$cargo->id}}">
+                                                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{$cargo->text}}
+                                            </option>
+                                        @endforeach
+                                    </optgroup>
+                                @endforeach
+                            </optgroup>
+                        @endforeach
+                    </optgroup>
+                @endforeach
+            </select>
+            @if ($errors->has("id_jefatura"))
+                <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first("id_jefatura") }}</strong>
+                    </span>
+            @endif
+        </div>
+    </div>
+
+
     @include("partials.file",["name"=>"adjuntos","title"=>"Descripción de Cargo","multiple"=>false ])
 @endsection
 @section("page_scripts")
@@ -87,6 +122,7 @@
                 });
             });
             $form.find("select[name='id_gerencia']").change(function (e) {
+                return;
                 let $select = $form.find("select[name='id_jefatura']");
                 $select.empty();
                 $select.trigger("change");
