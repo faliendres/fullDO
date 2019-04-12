c0b022796c482b1f5825b745965407f5f11e584d
Evelyn Ruiz <evelynruiz15@gmail.com>
2019-04-12 18:42:01 +0000

    remove column color from cargos table

--- resources/views/cargos/show.blade.php
+++ resources/views/cargos/show.blade.php
@@ -4,7 +4,6 @@
     @include("partials.field",["required"=>true,"name"=>"nombre","title"=>"Nombre"])
     @include("partials.textArea",["name"=>"descripcion","title"=>"Descripcion"])
     @include("partials.field",["name"=>"area","title"=>"Area"])
-    @include("partials.field",["type"=>"color","name"=>"color","title"=>"Color"])
     @include("partials.field",["type"=>"date","name"=>"desde","title"=>"Desde"])
     @include("partials.field",["type"=>"date","name"=>"hasta","title"=>"Hasta"])
 
