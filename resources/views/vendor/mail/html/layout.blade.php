37a39cec0222a65af5c2032e3f260f99790703ed
Carlos Mendez <carlos.mendez.marquez@gmail.com>
2019-04-24 14:38:26 -0400

    Cambios en estilo de email

--- resources/views/vendor/mail/html/layout.blade.php
+++ resources/views/vendor/mail/html/layout.blade.php
@@ -4,9 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
-</head>
-<body>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <style>
         .main-title {
             font-weight: lighter;
@@ -126,7 +124,8 @@
             }
         }
     </style>
-
+</head>
+<body>
     <table class="wrapper" width="100%" cellpadding="0" cellspacing="0">
         <tr>
             <td align="center">
