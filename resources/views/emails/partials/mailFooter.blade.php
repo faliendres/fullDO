55af0c90ee3f4d057725c3b87dc62d9c65839121
Evelyn Ruiz <evelynruiz15@gmail.com>
2019-04-26 15:01:22 +0000

    new mails

--- resources/views/emails/partials/mailFooter.blade.php
+++ resources/views/emails/partials/mailFooter.blade.php
@@ -1,20 +1,9 @@
-<div class="row" style="padding: 0 10%;
-            margin: 2em 0 0 0;">
-  <div class="column" style="float: left;
-          width: 50%;
-          padding: 5px;
-          text-align: center;">
-    <img src="{{ image_asset('empresas','logofulldo.png')}}">
-  </div>
-  <div class="column" style="float: left;
-          width: 50%;
-          padding: 5px;
-          text-align: center;">
-  	@if($logoEmpresa!='')
-    	<img src="{{ image_asset('empresas',$logoEmpresa) }}">
-    @endif
-  </div>
-</div>
+<table cellpadding="2" cellspacing="2" border="0" style="width: 100%; margin-top:50px"> 
+    <tr>
+        <td valign="top" style="text-align:center"><img src="{{ image_asset('empresas','logofulldo.png')}}" width="150" style="width:150px;"></td>
+        <td style="text-align:center"><img src="{{ image_asset('empresas',$logoEmpresa) }}" width="150" style="width:150px;"></td>
+    </tr>
+</table>
 <div class="footer">
 	<img src="https://i.ibb.co/3p6SH7D/footer-fd.png">
 </div>
